b7df8ed4a0820812349551671bbecca0f801ea1e
ferpalma <43299084+ferpalma@users.noreply.github.com>
2019-11-07 15:01:45 -0300

    Add files via upload

---
+++ ProjetoFarmacia/src/br/unitins/projetoFarmacia/model/Telefone.java
@@ -0,0 +1,32 @@
+package br.unitins.projetoFarmacia.model;
+
+public class Telefone {
+
+	private Integer idTelefone;
+	private String codigoArea;
+	private String numero;
+
+	public Integer getIdTelefone() {
+		return idTelefone;
+	}
+
+	public void setIdTelefone(Integer idTelefone) {
+		this.idTelefone = idTelefone;
+	}
+
+	public String getCodigoArea() {
+		return codigoArea;
+	}
+
+	public void setCodigoArea(String codigoArea) {
+		this.codigoArea = codigoArea;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+}
